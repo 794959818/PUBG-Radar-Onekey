f1fe4ecdabd6d90bcebed6192e15a5f9f7ab788e
chaojibinbin <38576100+chaojibinbin@users.noreply.github.com>
2018-04-23 13:00:31 +0800

    Update app.js

--- static/app.js
+++ static/app.js
@@ -684,18 +684,18 @@ const renderMap = () => {
       )
     } else { // enemy
       if (playerObj.team) {
-        label = `${playerObj.team}`
+        label = `队${playerObj.team}`
       } else if (playerObj.name) {
         label = playerObj.name
       } else {
         label = `<${playerObj.guid}>`
       }
       if (playerObj.kills) {
-        label += `(${playerObj.kills})`
+        label += `杀${playerObj.kills}`
       }
     }
     if (playerObj.health != null) {
-      label += `@${Math.floor(playerObj.health)}`
+      label += `血${Math.floor(playerObj.health)}`
     }
     feature.set('_label', label)
     // re-add should be fine
