6f829da4eb4a5f1e2d17ab12c1896d31fb023ec2
chaojibinbin <38576100+chaojibinbin@users.noreply.github.com>
2018-04-23 22:51:11 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "欢迎使用网页雷达一键脚本"
+echo "即将搭建的是随的4.23版本"
+echo "请输入交流群号"
+read -p "交流群： " qun
+
+curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
+source ~/.bash_profile
+nvm install v9.8.0
+nvm alias default v9.8.0
+yum -y install gcc-c++
+yum -y install flex
+yum -y install bison
+wget http://www.tcpdump.org/release/libpcap-1.8.1.tar.gz
+tar -zxvf libpcap-1.8.1.tar.gz
+cd libpcap-1.8.1
+./configure
+make
+make install
+
+
+
+yum install git
+git clone https://github.com/chaojibinbin/winnerpubg.git
+cd winnerpubg/
+npm i
+npm i -g pino
+npm install -g forever
+echo "请输入你的内网ip" 
+read -p "内网ip： " ip
+forever start index.js sniff eth0 $ip | pino
+wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+chmod +x shadowsocks-all.sh
+./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
+
+
+echo "搭建完成"
