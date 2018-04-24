522dd760b40d8211e4704766a90f737950d7be2c
chaojibinbin <38576100+chaojibinbin@users.noreply.github.com>
2018-04-24 13:08:57 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,8 +3,9 @@ echo "欢迎使用网页雷达一键脚本"
 echo "即将搭建的是随的4.23版本"
 echo "请输入交流群号"
 read -p "交流群： " qun
+echo "请输入你的内网ip" 
+read -p "内网ip： " ip
 
-cd root/
 curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
 source ~/.bash_profile
 nvm install v9.8.0
@@ -19,17 +20,14 @@ cd libpcap-1.8.1
 make
 make install
 
-cd winnnerpubg/
+
 npm i
 npm i -g pino
 npm install -g forever
-echo "请输入你的内网ip" 
-read -p "内网ip： " ip
-forever start index.js sniff eth0 $ip | pino
+
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
 ./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
-
+forever start index.js sniff eth0 $ip | pino
 
 echo "搭建完成"
-
